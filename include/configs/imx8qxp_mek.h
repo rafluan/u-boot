1a3b96ce343bc35ce20c1c2e38a39fa2e2f8ecea
Ye Li <ye.li@nxp.com>
2018-04-25 02:45:32 -0700

    MLK-18129-2 imx8qm/qxp: Add secure boot environment
    Add the secure boot relevant environment variables to MEK.
    When CONFIG_AHAB_BOOT is enabled, we will switch to boot in this
    new way:
    1. Load the OS container to address 0x98000000
    2. Using auth_cntr to authenticate the OS container. It will load the
       kernel and FDT to destination address.
    3. Using booti to boot kernel.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit 545c8fe112244f20377e97921389c86eacc8551f)
    (cherry picked from commit cba5acebbe6be0736f229ef719b1a1e5835e1a5f)
    (cherry picked from commit 07b9850354b27dafa4cc774c59770931af2ac898)
    (cherry picked from commit 149006de99c9c214232d30cc2e9795401766fe4b)
    (cherry picked from commit 6de1f4e766b547d7949ce720edd7b4ac0ed0752b)
    (cherry picked from commit e8ef9ddbf72abd9e304121592219e7d4f2c03f3b)

--- include/configs/imx8qxp_mek.h
+++ include/configs/imx8qxp_mek.h
@@ -44,6 +44,8 @@
 	"panel=NULL\0" \
 	"console=ttyLP0\0" \
 	"fdt_addr=0x83000000\0"			\
+	"cntr_addr=0x98000000\0"			\
+	"cntr_file=os_cntr_signed.bin\0" \
 	"boot_fdt=try\0" \
 	"fdt_file=undefined\0" \
 	"mmcdev="__stringify(CONFIG_ENV_MMC_DEVICE_INDEX)"\0" \
