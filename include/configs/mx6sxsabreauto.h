dcab2b71e2db18196176feab92250222531c0e58
Ye Li <ye.li@nxp.com>
2022-03-16 14:29:30 +0800

    LFU-278-12 mx6sxsabresd/auto: Enable the DM video splash screen
    Update the mx6sx SDB and AUTO ARD board codes and DTS to support
    DM video splash screen.
    Both have two video path:
    1. LCDIF1 -> LCD panel
    2. LCDIF2 -> LVDS channel 0
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- include/configs/mx6sxsabreauto.h
+++ include/configs/mx6sxsabreauto.h
@@ -64,7 +64,7 @@
 	CFG_MFG_ENV_SETTINGS \
 	TEE_ENV \
 	"tee_addr=0x84000000\0" \
-	"panel=Hannstar-XGA\0" \
+	"splashimage=0x8c000000\0" \
 	"fdt_addr=0x83000000\0" \
 	"console=ttymxc0\0" \
 	"bootargs=console=ttymxc0,115200 ubi.mtd=nandrootfs "  \
@@ -95,7 +95,7 @@
 	"tee_file=uTee-6sxauto\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
-	"panel=Hannstar-XGA\0" \
+	"splashimage=0x8c000000\0" \
 	"mmcdev="__stringify(CONFIG_ENV_MMC_DEVICE_INDEX)"\0" \
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk2p2 rootwait rw\0" \
