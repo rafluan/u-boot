1c2ff8e7a00953e319bbf8101fd4f0eaf51cc972
Ji Luo <ji.luo@nxp.com>
2023-05-23 14:44:02 +0800

    MA-21274 mmc: poll mmc status after sending rpmb request
    polling the mmc status to make sure the rpmb request has been
    sent and the mmc is not in BUSY state. This avoids potential
    rpmb request failure during rpmb data write.
    
    Change-Id: I3dee306631a51f109a2997e2976705f5a3a80f7d
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- drivers/mmc/rpmb.c
+++ drivers/mmc/rpmb.c
@@ -72,6 +72,7 @@ int mmc_rpmb_request(struct mmc *mmc, const struct s_rpmb *s,
 {
 	struct mmc_cmd cmd = {0};
 	struct mmc_data data;
+	int timeout_ms = 1000;
 	int ret;
 
 	ret = mmc_set_blockcount(mmc, count, is_rel_write);
@@ -98,6 +99,15 @@ int mmc_rpmb_request(struct mmc *mmc, const struct s_rpmb *s,
 #endif
 		return 1;
 	}
+
+	/* poll for the ready status */
+	if (mmc_poll_for_busy(mmc, timeout_ms)) {
+#ifdef CONFIG_MMC_RPMB_TRACE
+		printf("%s:mmc is busy!\n", __func__);
+#endif
+		return 1;
+	}
+
 	return 0;
 }
 int mmc_rpmb_response(struct mmc *mmc, struct s_rpmb *s,
