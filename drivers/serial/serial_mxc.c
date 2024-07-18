varigit/uboot-imx

serial: mxc: Make mxc_base modifiable by external code
Required when several boards use the same board header file
but use different UARTs as console.

drivers/serial/serial_mxc.c
@@ -184,7 +184,7 @@ static void _mxc_serial_setbrg(struct mxc_uart *base, unsigned long clk,
 #error "define CONFIG_MXC_UART_BASE to use the MXC UART driver"
 #endif
 
-#define mxc_base	((struct mxc_uart *)CONFIG_MXC_UART_BASE)
+struct mxc_uart *mxc_base = ((struct mxc_uart *)CONFIG_MXC_UART_BASE);
 
 static void mxc_serial_setbrg(void)
 {

rafluan/u-boot

Cherry-picked from 6b0b67ffe87017dfdaad9ac8a4b4e99ff5a5493d
Signed-off-by: Felix Radensky <felix.r@variscite.com>
(cherry picked from commit 9bb6c966f5a4b8970e71dbfd35d9e16e5ba7b576)

drivers/serial/serial_mxc.c
@@ -215,7 +215,7 @@ static void _mxc_serial_setbrg(struct mxc_uart *base, unsigned long clk,
 #error "define CFG_MXC_UART_BASE to use the MXC UART driver"
 #endif
 
-#define mxc_base	((struct mxc_uart *)CFG_MXC_UART_BASE)
+struct mxc_uart *mxc_base = ((struct mxc_uart *)CFG_MXC_UART_BASE);
 
 static void mxc_serial_setbrg(void)
 {