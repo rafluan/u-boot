5ae3e2c107c08d9ba3ac3240cda8413e3f5c83aa
Luan Rafael Carneiro <rafaeluan@yahoo.com.br>
2024-09-02 16:08:04 -0300

    imx8mm_var_dart: Fix update_info
    num_image_type_guids is decrepated, num_images should be used, otherwise
    capsule update will fail.
    
    Signed-off-by: Luan Rafael Carneiro <rafaeluan@yahoo.com.br>

--- board/variscite/imx8mm_var_dart/imx8mm_var_dart.c
+++ board/variscite/imx8mm_var_dart/imx8mm_var_dart.c
@@ -90,10 +90,10 @@ struct efi_fw_image fw_images[] = {
 
 struct efi_capsule_update_info update_info = {
 	.dfu_string = "mmc 2=1 raw 0x40 0x1000",
+	.num_images = ARRAY_SIZE(fw_images),
 	.images = fw_images,
 };
 
-u8 num_image_type_guids = ARRAY_SIZE(fw_images);
 #endif /* EFI_HAVE_CAPSULE_SUPPORT */
 
 int board_early_init_f(void)
