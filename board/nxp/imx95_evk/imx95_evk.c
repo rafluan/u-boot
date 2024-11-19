80876dec4a49c6d9dd4861560bc0f93cd1d1a032
Ranjani Vaidyanathan <ranjani.vaidyanathan@nxp.com>
2024-11-19 15:49:02 -0600

    LFU-817 iMX95: Fixup kernel and uboot dtb based on fuses
    Dynamically update the kernel uboot dtb based on fuses.
    Remove call to set the ARM perf level in SPL as System Manager
    already boots A55 at the max possible frequency based on fuse and
    pmic OTP.
    
    Signed-off-by: Ranjani Vaidyanathan <ranjani.vaidyanathan@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- board/nxp/imx95_evk/imx95_evk.c
+++ board/nxp/imx95_evk/imx95_evk.c
@@ -22,6 +22,8 @@
 #include <dm/uclass.h>
 #include <dm/uclass-internal.h>
 
+extern int board_fix_fdt_fuse(void *fdt);
+
 #ifdef CONFIG_USB_TCPC
 struct tcpc_port port;
 #ifdef CONFIG_TARGET_IMX95_15X15_EVK
@@ -558,6 +560,9 @@ static int board_fix_19x19_evk(void *fdt)
 
 int board_fix_fdt(void *fdt)
 {
+	/* Remove nodes based on fuses. */
+	board_fix_fdt_fuse(fdt);
+	
 #if IS_ENABLED(CONFIG_TARGET_IMX95_15X15_EVK)
 	return board_fix_15x15_evk(fdt);
 #else
