97f4dbd58a530c590dfca4a0c7b74858a5968247
Ye Li <ye.li@nxp.com>
2025-07-01 14:20:51 +0800

    LFU-898-10 imx94_evk: Disable XSPI1 node in kernel DTB
    Since XSPI EENV0 is only accessible by ELE when CRRM is enabled,
    kernel driver can't work for this case, so disable it for CRRM.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- board/nxp/imx94_evk/imx94_evk.c
+++ board/nxp/imx94_evk/imx94_evk.c
@@ -243,6 +243,28 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 		return ret;
 	}
 
+	/* Disable XSPI1 node for CRRM */
+#if IS_ENABLED(CONFIG_IMX_CRRM)
+	int nodeoff;
+	const char *status = "disabled";
+
+	nodeoff = fdt_path_offset(blob, "/soc/bus@42800000/spi@42b90000");
+	if (nodeoff > 0) {
+		ret = fdt_increase_size(blob, 256);
+		if (ret) {
+			printf("Unable to increase fdt size, err=%s\n", fdt_strerror(ret));
+			return ret;
+		}
+
+		ret = fdt_setprop(blob, nodeoff, "status", status,
+				  strlen(status) + 1);
+		if (ret) {
+			printf("Unable to disable XSPI1, err=%s\n", fdt_strerror(ret));
+			return ret;
+		}
+	}
+#endif
+
 	return 0;
 }
 #endif
