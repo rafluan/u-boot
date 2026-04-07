nxp-imx/uboot-imx

MLK-23574-13 usb: gadget: Add the cadence USB3 gadget driver
This driver is ported from NXP i.MX U-Boot version imx_v2019.04
and some changes have also been made to adapt to U-Boot.

Add the Cadence USB3 IP(CDNS3) driver for the gadget (device mode).
The CDNS3 gadget driver support DM mode. CONFIG_DM_USB_GADGET should
be enabled when use this driver.

Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
Signed-off-by: Ye Li <ye.li@nxp.com>

drivers/usb/cdns3/cdns3-generic.c
@@ -1,0 +1,114 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2019 NXP
+ */
+
+#include <common.h>
+#include <clk.h>
+#include <dm.h>
+#include <generic-phy.h>
+#include <linux/usb/gadget.h>
+#include <dm/device-internal.h>
+#include "core.h"
+#include "gadget.h"
+
+static int cdns3_generic_peripheral_clk_init(struct udevice *dev,
+					     struct cdns3_generic_peripheral
+					     *priv)
+{
+#if CONFIG_IS_ENABLED(CLK)
+	int ret;
+
+	ret = clk_get_bulk(dev, &priv->clks);
+	if (ret == -ENOSYS)
+		return 0;
+	if (ret)
+		return ret;
+
+	ret = clk_enable_bulk(&priv->clks);
+	if (ret) {
+		clk_release_bulk(&priv->clks);
+		return ret;
+	}
+#endif
+
+	return 0;
+}
+
+int dm_usb_gadget_handle_interrupts(struct udevice *dev)
+{
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3_role_irq_handler(cdns3);
+
+	return 0;
+}
+
+static int cdns3_generic_peripheral_probe(struct udevice *dev)
+{
+	int ret;
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3->dev = dev;
+
+	ret = generic_phy_get_by_index(dev, 0, &priv->phy);
+	if (ret && ret != -ENOENT) {
+		printf("Failed to get USB PHY for %s\n", dev->name);
+		return ret;
+	}
+
+	ret = cdns3_generic_peripheral_clk_init(dev, priv);
+	if (ret)
+		return ret;
+
+	ret = cdns3_init(cdns3);
+
+	return 0;
+}
+
+static int cdns3_generic_peripheral_remove(struct udevice *dev)
+{
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3_exit(cdns3);
+
+	clk_release_bulk(&priv->clks);
+
+	if (generic_phy_valid(&priv->phy))
+		device_remove(priv->phy.dev, DM_REMOVE_NORMAL);
+
+	return 0;
+}
+
+static int cdns3_generic_peripheral_ofdata_to_platdata(struct udevice *dev)
+{
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3->none_core_regs = (void __iomem *)devfdt_get_addr_name(dev,
+								     "none-core");
+	cdns3->xhci_regs = (void __iomem *)devfdt_get_addr_name(dev, "xhci");
+	cdns3->dev_regs = (void __iomem *)devfdt_get_addr_name(dev, "dev");
+	cdns3->phy_regs = (void __iomem *)devfdt_get_addr_name(dev, "phy");
+	cdns3->otg_regs = (void __iomem *)devfdt_get_addr_name(dev, "otg");
+
+	return 0;
+}
+
+static const struct udevice_id cdns3_generic_peripheral_ids[] = {
+	{ .compatible = "Cadence,usb3" },
+	{},
+};
+
+U_BOOT_DRIVER(cdns3_generic_peripheral) = {
+	.name	= "cdns3-generic-peripheral",
+	.id	= UCLASS_USB_GADGET_GENERIC,
+	.of_match = cdns3_generic_peripheral_ids,
+	.ofdata_to_platdata = cdns3_generic_peripheral_ofdata_to_platdata,
+	.probe = cdns3_generic_peripheral_probe,
+	.remove = cdns3_generic_peripheral_remove,
+	.priv_auto_alloc_size = sizeof(struct cdns3_generic_peripheral),
+};

drivers/usb/cdns3/core.c
@@ -1,0 +1,203 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016 Cadence Design Systems - https://www.cadence.com/
+ * Copyright 2019 NXP
+ */
+#include <common.h>
+#include <malloc.h>
+#include <wait_bit.h>
+#include <asm/dma-mapping.h>
+#include <asm/io.h>
+#include <linux/bug.h>
+#include <linux/list.h>
+#include <linux/compat.h>
+#include <linux/usb/gadget.h>
+#include <linux/usb/otg.h>
+
+#include "cdns3-nxp-reg-def.h"
+#include "core.h"
+#include "gadget-export.h"
+#include "gadget.h"
+
+static void cdns3_reset_core(struct cdns3 *cdns)
+{
+	/* Set all Reset bits */
+	setbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1, ALL_SW_RESET);
+	udelay(1);
+}
+
+static int cdns3_host_role_set(struct cdns3 *cdns)
+{
+	int ret;
+
+	struct cdns3_generic_peripheral *priv = container_of(cdns,
+			struct cdns3_generic_peripheral, cdns3);
+
+	clrsetbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+			MODE_STRAP_MASK, HOST_MODE | OC_DISABLE);
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     PHYAHB_SW_RESET);
+	mdelay(1);
+	generic_phy_init(&priv->phy);
+	setbits_le32(cdns->phy_regs + TB_ADDR_TX_RCVDETSC_CTRL,
+		     RXDET_IN_P3_32KHZ);
+	udelay(10);
+	/* Force B Session Valid as 1 */
+	writel(SET_FORCE_B_SESS_VALID, cdns->phy_regs + USB2_PHY_AFE_BC_REG4);
+	mdelay(1);
+
+	setbits_le32(cdns->none_core_regs + USB3_INT_REG, HOST_INT1_EN);
+
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     ALL_SW_RESET);
+
+	dev_dbg(cdns->dev, "wait xhci_power_on_ready\n");
+	ret = wait_for_bit_le32(cdns->none_core_regs + USB3_CORE_STATUS,
+				HOST_POWER_ON_READY, true, 100, false);
+	if (ret) {
+		dev_err(cdns->dev, "wait xhci_power_on_ready timeout\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int cdns3_gadget_role_set(struct cdns3 *cdns)
+{
+	int ret;
+
+	struct cdns3_generic_peripheral *priv = container_of(cdns,
+			struct cdns3_generic_peripheral, cdns3);
+
+	clrsetbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+			MODE_STRAP_MASK, DEV_MODE);
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     PHYAHB_SW_RESET);
+
+	generic_phy_init(&priv->phy);
+	setbits_le32(cdns->phy_regs + TB_ADDR_TX_RCVDETSC_CTRL,
+		     RXDET_IN_P3_32KHZ);
+	udelay(10);
+	/* Force B Session Valid as 1 */
+	writel(SET_FORCE_B_SESS_VALID, cdns->phy_regs + USB2_PHY_AFE_BC_REG4);
+	setbits_le32(cdns->none_core_regs + USB3_INT_REG, DEV_INT_EN);
+
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     ALL_SW_RESET);
+
+	dev_dbg(cdns->dev, "wait gadget_power_on_ready\n");
+	ret = wait_for_bit_le32(cdns->none_core_regs + USB3_CORE_STATUS,
+				DEV_POWER_ON_READY, true, 100, false);
+	if (ret) {
+		dev_err(cdns->dev, "wait gadget_power_on_ready timeout\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int cdns3_set_role(struct cdns3 *cdns, enum cdns3_roles role)
+{
+	int ret;
+
+	if (role == CDNS3_ROLE_END)
+		return -EPERM;
+
+	/* Wait clk value */
+	writel(CLK_VLD, cdns->none_core_regs + USB3_SSPHY_STATUS);
+	ret = wait_for_bit_le32(cdns->none_core_regs + USB3_SSPHY_STATUS,
+				CLK_VLD, true, 100, false);
+	if (ret) {
+		dev_err(cdns->dev, "wait clkvld timeout\n");
+		return ret;
+	}
+
+	cdns3_reset_core(cdns);
+
+	if (role == CDNS3_ROLE_HOST) {
+		cdns3_host_role_set(cdns);
+		dev_dbg(cdns->dev, "switch to host role successfully\n");
+	} else { /* gadget mode */
+		cdns3_gadget_role_set(cdns);
+		dev_dbg(cdns->dev, "switch to gadget role successfully\n");
+	}
+
+	return 0;
+}
+
+static enum cdns3_roles cdns3_get_role(struct cdns3 *cdns)
+{
+	return cdns->roles[CDNS3_ROLE_HOST]
+		? CDNS3_ROLE_HOST
+		: CDNS3_ROLE_GADGET;
+}
+
+/**
+ * cdns3_core_init_role - initialize role of operation
+ * @cdns: Pointer to cdns3 structure
+ * @dr_mode: Role mode of device
+ *
+ * Returns 0 on success otherwise negative errno
+ */
+static int cdns3_core_init_role(struct cdns3 *cdns, enum usb_dr_mode dr_mode)
+{
+	cdns->role = CDNS3_ROLE_END;
+	if (dr_mode == USB_DR_MODE_UNKNOWN)
+		dr_mode = USB_DR_MODE_OTG;
+
+	/* Currently, only support gadget mode */
+	if (dr_mode == USB_DR_MODE_OTG || dr_mode == USB_DR_MODE_HOST) {
+		dev_err(cdns->dev, "doesn't support host and OTG, only for gadget\n");
+		return -EPERM;
+	}
+
+	if (dr_mode == USB_DR_MODE_PERIPHERAL) {
+		if (cdns3_gadget_init(cdns))
+			dev_info(cdns->dev, "doesn't support gadget\n");
+	}
+
+	if (!cdns->roles[CDNS3_ROLE_HOST] && !cdns->roles[CDNS3_ROLE_GADGET]) {
+		dev_err(cdns->dev, "no supported roles\n");
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+static void cdns3_remove_roles(struct cdns3 *cdns)
+{
+	/* Only support gadget */
+	cdns3_gadget_remove(cdns);
+}
+
+int cdns3_init(struct cdns3 *cdns)
+{
+	int ret;
+
+	ret = cdns3_core_init_role(cdns, USB_DR_MODE_PERIPHERAL);
+
+	cdns->role = cdns3_get_role(cdns);
+	dev_dbg(dev, "the init role is %d\n", cdns->role);
+	cdns3_set_role(cdns, cdns->role);
+	ret = cdns3_role_start(cdns, cdns->role);
+	if (ret) {
+		dev_err(dev, "can't start %s role\n", cdns3_role(cdns)->name);
+		goto err;
+	}
+
+	dev_dbg(dev, "Cadence USB3 core: probe succeed\n");
+
+	return 0;
+
+err:
+	cdns3_remove_roles(cdns);
+
+	return ret;
+}
+
+void cdns3_exit(struct cdns3 *cdns)
+{
+	cdns3_role_stop(cdns);
+	cdns3_remove_roles(cdns);
+	cdns3_reset_core(cdns);
+}

rafluan/u-boot

(cherry picked from commit 09d22730a9b022dd6354740d46c6fa155553bbd0)
(cherry picked from commit 7069c6cc75111ff35b61278e768abcd7cb2b6cb1)

drivers/usb/cdns3/cdns3-generic.c
@@ -1,0 +1,119 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2019 NXP
+ */
+
+#include <clk.h>
+#include <dm.h>
+#include <generic-phy.h>
+#include <linux/usb/gadget.h>
+#include <linux/bug.h>
+#include <dm/device-internal.h>
+#include "core.h"
+#include "gadget.h"
+
+static int cdns3_generic_peripheral_clk_init(struct udevice *dev,
+					     struct cdns3_generic_peripheral
+					     *priv)
+{
+#if CONFIG_IS_ENABLED(CLK)
+	int ret;
+
+	ret = clk_get_bulk(dev, &priv->clks);
+	if (ret == -ENOSYS)
+		return 0;
+	if (ret)
+		return ret;
+
+	ret = clk_enable_bulk(&priv->clks);
+	if (ret) {
+		clk_release_bulk(&priv->clks);
+		return ret;
+	}
+#endif
+
+	return 0;
+}
+
+static int cdns3_generic_handle_interrupts(struct udevice *dev)
+{
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3_role_irq_handler(cdns3);
+
+	return 0;
+}
+
+static int cdns3_generic_peripheral_probe(struct udevice *dev)
+{
+	int ret;
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3->dev = dev;
+
+	ret = generic_phy_get_by_index(dev, 0, &priv->phy);
+	if (ret && ret != -ENOENT) {
+		printf("Failed to get USB PHY for %s\n", dev->name);
+		return ret;
+	}
+
+	ret = cdns3_generic_peripheral_clk_init(dev, priv);
+	if (ret)
+		return ret;
+
+	ret = cdns3_init(cdns3);
+
+	return 0;
+}
+
+static int cdns3_generic_peripheral_remove(struct udevice *dev)
+{
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3_exit(cdns3);
+
+	clk_release_bulk(&priv->clks);
+
+	if (generic_phy_valid(&priv->phy))
+		device_remove(priv->phy.dev, DM_REMOVE_NORMAL);
+
+	return 0;
+}
+
+static int cdns3_generic_peripheral_ofdata_to_platdata(struct udevice *dev)
+{
+	struct cdns3_generic_peripheral *priv = dev_get_priv(dev);
+	struct cdns3 *cdns3 = &priv->cdns3;
+
+	cdns3->none_core_regs = (void __iomem *)devfdt_get_addr_name(dev,
+								     "none-core");
+	cdns3->xhci_regs = (void __iomem *)devfdt_get_addr_name(dev, "xhci");
+	cdns3->dev_regs = (void __iomem *)devfdt_get_addr_name(dev, "dev");
+	cdns3->phy_regs = (void __iomem *)devfdt_get_addr_name(dev, "phy");
+	cdns3->otg_regs = (void __iomem *)devfdt_get_addr_name(dev, "otg");
+
+	return 0;
+}
+
+static const struct udevice_id cdns3_generic_peripheral_ids[] = {
+	{ .compatible = "Cadence,usb3" },
+	{},
+};
+
+static const struct usb_gadget_generic_ops cdns3_gadget_ops = {
+	.handle_interrupts	= cdns3_generic_handle_interrupts,
+};
+
+U_BOOT_DRIVER(cdns3_generic_peripheral) = {
+	.name	= "cdns3-generic-peripheral",
+	.id	= UCLASS_USB_GADGET_GENERIC,
+	.of_match = cdns3_generic_peripheral_ids,
+	.of_to_plat = cdns3_generic_peripheral_ofdata_to_platdata,
+	.probe = cdns3_generic_peripheral_probe,
+	.remove = cdns3_generic_peripheral_remove,
+	.ops	= &cdns3_gadget_ops,
+	.priv_auto = sizeof(struct cdns3_generic_peripheral),
+};

drivers/usb/cdns3/core.c
@@ -1,0 +1,204 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016 Cadence Design Systems - https://www.cadence.com/
+ * Copyright 2019 NXP
+ */
+#include <malloc.h>
+#include <wait_bit.h>
+#include <asm/dma-mapping.h>
+#include <asm/io.h>
+#include <linux/bug.h>
+#include <linux/list.h>
+#include <linux/compat.h>
+#include <linux/usb/gadget.h>
+#include <linux/usb/otg.h>
+#include <dm.h>
+#include <dm/device_compat.h>
+
+#include "cdns3-nxp-reg-def.h"
+#include "core.h"
+#include "gadget-export.h"
+#include "gadget.h"
+
+static void cdns3_reset_core(struct cdns3 *cdns)
+{
+	/* Set all Reset bits */
+	setbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1, ALL_SW_RESET);
+	udelay(1);
+}
+
+static int cdns3_host_role_set(struct cdns3 *cdns)
+{
+	int ret;
+
+	struct cdns3_generic_peripheral *priv = container_of(cdns,
+			struct cdns3_generic_peripheral, cdns3);
+
+	clrsetbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+			MODE_STRAP_MASK, HOST_MODE | OC_DISABLE);
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     PHYAHB_SW_RESET);
+	mdelay(1);
+	generic_phy_init(&priv->phy);
+	setbits_le32(cdns->phy_regs + TB_ADDR_TX_RCVDETSC_CTRL,
+		     RXDET_IN_P3_32KHZ);
+	udelay(10);
+	/* Force B Session Valid as 1 */
+	writel(SET_FORCE_B_SESS_VALID, cdns->phy_regs + USB2_PHY_AFE_BC_REG4);
+	mdelay(1);
+
+	setbits_le32(cdns->none_core_regs + USB3_INT_REG, HOST_INT1_EN);
+
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     ALL_SW_RESET);
+
+	dev_dbg(cdns->dev, "wait xhci_power_on_ready\n");
+	ret = wait_for_bit_le32(cdns->none_core_regs + USB3_CORE_STATUS,
+				HOST_POWER_ON_READY, true, 100, false);
+	if (ret) {
+		dev_err(cdns->dev, "wait xhci_power_on_ready timeout\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int cdns3_gadget_role_set(struct cdns3 *cdns)
+{
+	int ret;
+
+	struct cdns3_generic_peripheral *priv = container_of(cdns,
+			struct cdns3_generic_peripheral, cdns3);
+
+	clrsetbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+			MODE_STRAP_MASK, DEV_MODE);
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     PHYAHB_SW_RESET);
+
+	generic_phy_init(&priv->phy);
+	setbits_le32(cdns->phy_regs + TB_ADDR_TX_RCVDETSC_CTRL,
+		     RXDET_IN_P3_32KHZ);
+	udelay(10);
+	/* Force B Session Valid as 1 */
+	writel(SET_FORCE_B_SESS_VALID, cdns->phy_regs + USB2_PHY_AFE_BC_REG4);
+	setbits_le32(cdns->none_core_regs + USB3_INT_REG, DEV_INT_EN);
+
+	clrbits_le32(cdns->none_core_regs + USB3_CORE_CTRL1,
+		     ALL_SW_RESET);
+
+	dev_dbg(cdns->dev, "wait gadget_power_on_ready\n");
+	ret = wait_for_bit_le32(cdns->none_core_regs + USB3_CORE_STATUS,
+				DEV_POWER_ON_READY, true, 100, false);
+	if (ret) {
+		dev_err(cdns->dev, "wait gadget_power_on_ready timeout\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int cdns3_set_role(struct cdns3 *cdns, enum cdns3_roles role)
+{
+	int ret;
+
+	if (role == CDNS3_ROLE_END)
+		return -EPERM;
+
+	/* Wait clk value */
+	writel(CLK_VLD, cdns->none_core_regs + USB3_SSPHY_STATUS);
+	ret = wait_for_bit_le32(cdns->none_core_regs + USB3_SSPHY_STATUS,
+				CLK_VLD, true, 100, false);
+	if (ret) {
+		dev_err(cdns->dev, "wait clkvld timeout\n");
+		return ret;
+	}
+
+	cdns3_reset_core(cdns);
+
+	if (role == CDNS3_ROLE_HOST) {
+		cdns3_host_role_set(cdns);
+		dev_dbg(cdns->dev, "switch to host role successfully\n");
+	} else { /* gadget mode */
+		cdns3_gadget_role_set(cdns);
+		dev_dbg(cdns->dev, "switch to gadget role successfully\n");
+	}
+
+	return 0;
+}
+
+static enum cdns3_roles cdns3_get_role(struct cdns3 *cdns)
+{
+	return cdns->roles[CDNS3_ROLE_HOST]
+		? CDNS3_ROLE_HOST
+		: CDNS3_ROLE_GADGET;
+}
+
+/**
+ * cdns3_core_init_role - initialize role of operation
+ * @cdns: Pointer to cdns3 structure
+ * @dr_mode: Role mode of device
+ *
+ * Returns 0 on success otherwise negative errno
+ */
+static int cdns3_core_init_role(struct cdns3 *cdns, enum usb_dr_mode dr_mode)
+{
+	cdns->role = CDNS3_ROLE_END;
+	if (dr_mode == USB_DR_MODE_UNKNOWN)
+		dr_mode = USB_DR_MODE_OTG;
+
+	/* Currently, only support gadget mode */
+	if (dr_mode == USB_DR_MODE_OTG || dr_mode == USB_DR_MODE_HOST) {
+		dev_err(cdns->dev, "doesn't support host and OTG, only for gadget\n");
+		return -EPERM;
+	}
+
+	if (dr_mode == USB_DR_MODE_PERIPHERAL) {
+		if (cdns3_gadget_init(cdns))
+			dev_info(cdns->dev, "doesn't support gadget\n");
+	}
+
+	if (!cdns->roles[CDNS3_ROLE_HOST] && !cdns->roles[CDNS3_ROLE_GADGET]) {
+		dev_err(cdns->dev, "no supported roles\n");
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+static void cdns3_remove_roles(struct cdns3 *cdns)
+{
+	/* Only support gadget */
+	cdns3_gadget_remove(cdns);
+}
+
+int cdns3_init(struct cdns3 *cdns)
+{
+	int ret;
+
+	ret = cdns3_core_init_role(cdns, USB_DR_MODE_PERIPHERAL);
+
+	cdns->role = cdns3_get_role(cdns);
+	dev_dbg(cdns->dev, "the init role is %d\n", cdns->role);
+	cdns3_set_role(cdns, cdns->role);
+	ret = cdns3_role_start(cdns, cdns->role);
+	if (ret) {
+		dev_err(cdns->dev, "can't start %s role\n", cdns3_role(cdns)->name);
+		goto err;
+	}
+
+	dev_dbg(cdns->dev, "Cadence USB3 core: probe succeed\n");
+
+	return 0;
+
+err:
+	cdns3_remove_roles(cdns);
+
+	return ret;
+}
+
+void cdns3_exit(struct cdns3 *cdns)
+{
+	cdns3_role_stop(cdns);
+	cdns3_remove_roles(cdns);
+	cdns3_reset_core(cdns);
+}