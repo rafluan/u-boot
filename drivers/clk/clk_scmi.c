rafluan/u-boot

commit 3547e315c188cdbb24312543d67d132b113eb01e

clk: scmi: Defer issue of SCMI_CLOCK_ATTRIBUTES
Instead of resolving clock control flags using SCMI_CLOCK_ATTRIBUTES
during probe for each and every clock, resolve the clock control
flags using SCMI_CLOCK_ATTRIBUTES when the clock control flags are
first used. Because most clock are never used by U-Boot, this allows
reducing the amount of SCMI_CLOCK_ATTRIBUTES considerably, and this
improve probe time of the scmi clock driver and U-Boot start up time.

On Renesas X5H, with 1700+ SCMI clock, the boot time improved by 1.7s .

Reviewed-by: Peng Fan <peng.fan@nxp.com>
Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

drivers/clk/clk_scmi.c
@@ -19,6 +19,7 @@ struct clk_scmi {
 	struct clk clk;
 	char name[SCMI_CLOCK_NAME_LENGTH_MAX];
 	u32 ctrl_flags;
+	bool attrs_resolved;
 };
 
 struct scmi_clock_priv {
@@ -86,7 +87,7 @@ static int scmi_clk_get_num_clock(struct udevice *dev, size_t *num_clocks)
 	return 0;
 }
 
-static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char **name,
+static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char *name,
 				 u32 *attr)
 {
 	struct scmi_clock_priv *priv = dev_get_priv(dev);
@@ -110,7 +111,7 @@ static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char **name,
 		if (ret)
 			return ret;
 
-		*name = strdup(out.clock_name);
+		strncpy(name, out.clock_name, SCMI_CLOCK_NAME_LENGTH_MAX);
 		*attr = out.attributes;
 	} else {
 		struct scmi_clk_attribute_out out;
@@ -127,7 +128,7 @@ static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char **name,
 		if (ret)
 			return ret;
 
-		*name = strdup(out.clock_name);
+		strncpy(name, out.clock_name, SCMI_CLOCK_NAME_LENGTH_MAX);
 		*attr = out.attributes;
 	}
 
@@ -167,6 +168,8 @@ static int scmi_clk_gate(struct clk *clk, int enable)
 static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
 {
 	struct clk_scmi *clkscmi;
+	struct udevice *dev;
+	u32 attributes;
 	struct clk *c;
 	int ret;
 
@@ -174,7 +177,34 @@ static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
 	if (ret)
 		return ret;
 
+	dev = c->dev->parent;
+
 	clkscmi = container_of(c, struct clk_scmi, clk);
+
+	if (!clkscmi->attrs_resolved) {
+		char name[SCMI_CLOCK_NAME_LENGTH_MAX];
+		ret = scmi_clk_get_attibute(dev, clk->id & CLK_ID_MSK,
+					    name, &attributes);
+		if (ret)
+			return ret;
+
+		strncpy(clkscmi->name, name, SCMI_CLOCK_NAME_LENGTH_MAX);
+		if (CLK_HAS_RESTRICTIONS(attributes)) {
+			u32 perm;
+
+			ret = scmi_clk_get_permissions(dev, clk->id & CLK_ID_MSK, &perm);
+			if (ret < 0)
+				clkscmi->ctrl_flags = 0;
+			else
+				clkscmi->ctrl_flags = perm;
+		} else {
+			clkscmi->ctrl_flags = SUPPORT_CLK_STAT_CONTROL |
+					      SUPPORT_CLK_PARENT_CONTROL |
+					      SUPPORT_CLK_RATE_CONTROL;
+		}
+
+		clkscmi->attrs_resolved = true;
+	}
 
 	*ctrl_flags = clkscmi->ctrl_flags;
 
@@ -328,7 +358,6 @@ static int scmi_clk_probe(struct udevice *dev)
 	for (i = 0; i < num_clocks; i++) {
 		clk_scmi = clk_scmi_bulk + i;
 		char *clock_name = clk_scmi->name;
-		u32 attributes;
 
 		snprintf(clock_name, SCMI_CLOCK_NAME_LENGTH_MAX, "scmi-%zu", i);
 
@@ -339,21 +368,6 @@ static int scmi_clk_probe(struct udevice *dev)
 
 		dev_clk_dm(dev, i, &clk_scmi->clk);
 		dev_set_parent_priv(clk_scmi->clk.dev, priv);
-
-		if (!scmi_clk_get_attibute(dev, i, &clock_name, &attributes)) {
-			if (CLK_HAS_RESTRICTIONS(attributes)) {
-				u32 perm;
-
-				ret = scmi_clk_get_permissions(dev, i, &perm);
-				if (ret < 0)
-					clk_scmi->ctrl_flags = 0;
-				else
-					clk_scmi->ctrl_flags = perm;
-			} else {
-				clk_scmi->ctrl_flags = SUPPORT_CLK_STAT_CONTROL | SUPPORT_CLK_PARENT_CONTROL |
-						       SUPPORT_CLK_RATE_CONTROL;
-			}
-		}
 	}
 
 	return 0;

commit 0a64d2be21c9c80348595486c1ba6dc48bd61238

clk: scmi: export scmi_clk_resolve_attr
With commit 3547e315c188 ("clk: scmi: Defer issue of SCMI_CLOCK_ATTRIBUTES"),
the SCMI clock names might be defered. However the SCMI clock may be
input to other clock controllers, such as clk-imx95-blkctrl.

Without the clock names being resolved, there will be error:
clk_register: failed to get hsiopll device (parent of hsio_blk_ctl_clk)

So export this API for users.

Fixes: 3547e315c188 ("clk: scmi: Defer issue of SCMI_CLOCK_ATTRIBUTES"),
Signed-off-by: Peng Fan <peng.fan@nxp.com>
Reviewed-by: Patrice Chotard <patrice.chotard@foss.st.com>

drivers/clk/clk_scmi.c
@@ -6,6 +6,7 @@
 #define LOG_CATEGORY UCLASS_CLK
 
 #include <clk-uclass.h>
+#include <clk/scmi.h>
 #include <dm.h>
 #include <dm/device_compat.h>
 #include <dm/device-internal.h>
@@ -175,7 +176,7 @@ static int scmi_clk_gate(struct clk *clk, int enable)
 	return scmi_to_linux_errno(out.status);
 }
 
-static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
+int scmi_clk_resolve_attr(ulong id, u32 *ctrl_flags)
 {
 	struct clk_scmi *clkscmi;
 	struct udevice *dev;
@@ -183,7 +184,7 @@ static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
 	struct clk *c;
 	int ret;
 
-	ret = clk_get_by_id(clk->id, &c);
+	ret = clk_get_by_id(id, &c);
 	if (ret)
 		return ret;
 
@@ -193,7 +194,7 @@ static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
 
 	if (!clkscmi->attrs_resolved) {
 		char name[SCMI_CLOCK_NAME_LENGTH_MAX];
-		ret = scmi_clk_get_attribute(dev, clk->id & CLK_ID_MSK,
+		ret = scmi_clk_get_attribute(dev, id & CLK_ID_MSK,
 					     name, &attributes);
 		if (ret)
 			return ret;
@@ -202,7 +203,7 @@ static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
 		if (CLK_HAS_RESTRICTIONS(attributes)) {
 			u32 perm;
 
-			ret = scmi_clk_get_permissions(dev, clk->id & CLK_ID_MSK, &perm);
+			ret = scmi_clk_get_permissions(dev, id & CLK_ID_MSK, &perm);
 			if (ret < 0)
 				clkscmi->ctrl_flags = 0;
 			else
@@ -216,7 +217,8 @@ static int scmi_clk_get_ctrl_flags(struct clk *clk, u32 *ctrl_flags)
 		clkscmi->attrs_resolved = true;
 	}
 
-	*ctrl_flags = clkscmi->ctrl_flags;
+	if (ctrl_flags)
+		*ctrl_flags = clkscmi->ctrl_flags;
 
 	return 0;
 }
@@ -229,7 +231,7 @@ static int scmi_clk_enable(struct clk *clk)
 	if (!CONFIG_IS_ENABLED(CLK_CCF))
 		return scmi_clk_gate(clk, 1);
 
-	ret = scmi_clk_get_ctrl_flags(clk, &ctrl_flags);
+	ret = scmi_clk_resolve_attr(clk->id, &ctrl_flags);
 	if (ret)
 		return ret;
 
@@ -249,7 +251,7 @@ static int scmi_clk_disable(struct clk *clk)
 	if (!CONFIG_IS_ENABLED(CLK_CCF))
 		return scmi_clk_gate(clk, 0);
 
-	ret = scmi_clk_get_ctrl_flags(clk, &ctrl_flags);
+	ret = scmi_clk_resolve_attr(clk->id, &ctrl_flags);
 	if (ret)
 		return ret;
 
@@ -317,7 +319,7 @@ static ulong scmi_clk_set_rate(struct clk *clk, ulong rate)
 	if (!CONFIG_IS_ENABLED(CLK_CCF))
 		return __scmi_clk_set_rate(clk, rate);
 
-	ret = scmi_clk_get_ctrl_flags(clk, &ctrl_flags);
+	ret = scmi_clk_resolve_attr(clk->id, &ctrl_flags);
 	if (ret)
 		return ret;
 
@@ -407,7 +409,7 @@ static int scmi_clk_set_parent(struct clk *clk, struct clk *parent)
 	if (!CONFIG_IS_ENABLED(CLK_CCF))
 		return __scmi_clk_set_parent(clk, parent);
 
-	ret = scmi_clk_get_ctrl_flags(clk, &ctrl_flags);
+	ret = scmi_clk_resolve_attr(clk->id, &ctrl_flags);
 	if (ret)
 		return ret;
 

include/clk/scmi.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2025 NXP
+ */
+
+#ifndef _CLK_SCMI_H
+#define _CLK_SCMI_H
+
+#include <linux/types.h>
+
+int scmi_clk_resolve_attr(ulong id, u32 *ctrl_flags);
+#endif