nxp-imx/uboot-imx

MLK-16753-9 imx: mx7: add optee support
Add different defconfigs.
Enable Trustzone.
Update env to runtime boot OP-TEE.
mx7d arm2 board not supported now.

Signed-off-by: Peng Fan <peng.fan@nxp.com>

board/freescale/mx7dsabresd/mx7dsabresd.c
@@ -648,6 +648,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

rafluan/u-boot

(cherry picked from commit 435807b41ae99bb9f4e0b2c7b9e5287c2bd520c0)
(cherry picked from commit b78124c2f68b9aa4ec2f160d99364bfab72ab5e3)
(cherry picked from commit 641b195a5633cfc3960a42bbb332dd8fead9f939)
(cherry picked from commit 4ca1af73ade293f135ccf44686f87e39b1f494ec)
(cherry picked from commit eaff76c0f64ca756952c91a923a7bbeeffa36107)
(cherry picked from commit 44b2dafa6b97cc6b4ac0fe6a16010212f6dc9a81)
(cherry picked from commit b8a1a0f700630338d4dbb3c108b9afb6e4fb3238)
(cherry picked from commit f3a189dd4489546bde0d437794d44c262a7ebe6b)

board/nxp/mx7dsabresd/mx7dsabresd.c
@@ -676,6 +676,11 @@ int board_late_init(void)
 	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
 	unsigned char eth1addr[6];
 
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #if CONFIG_IS_ENABLED(ENV_IS_IN_MMC)
 	board_late_mmc_env_init();
 #endif