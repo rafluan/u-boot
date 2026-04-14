nxp-imx/uboot-imx

MLK-18906-3: imx8mm can choose emmc device by uuu
uuu can change emmc device number for fastboot

Signed-off-by: Frank Li <Frank.Li@nxp.com>

arch/arm/mach-imx/mmc_env.c
@@ -24,7 +24,7 @@ int mmc_get_env_dev(void)
 
 	/* If not boot from sd/mmc, use default value */
 	if ((boot_type != BOOT_TYPE_SD) && (boot_type != BOOT_TYPE_MMC))
-		return CONFIG_SYS_MMC_ENV_DEV;
+		return env_get_ulong("mmcdev", 10, CONFIG_SYS_MMC_ENV_DEV);
 
 	return board_mmc_get_env_dev(devno);
 }

rafluan/u-boot

(cherry picked from commit 4da1ab91f864b804ad561e04f44b618ec86b99b7)
(cherry picked from commit 922533b57d134d68817dbadc62314e3345438606)
(cherry picked from commit eb5edae29d75395ce72a3e6dc39b2b004b3ec8d8)
(cherry picked from commit 460931c87c498ca2710e8ce9f23ae2d40c385b6f)
(cherry picked from commit b2fbb432d956402556f91ad86f0553a21ede53e5)

arch/arm/mach-imx/mmc_env.c
@@ -7,6 +7,7 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/io.h>
 #include <asm/mach-imx/boot_mode.h>
+#include <env.h>
 
 __weak int board_mmc_get_env_dev(int devno)
 {
@@ -22,7 +23,7 @@ int mmc_get_env_dev(void)
 
 	/* If not boot from sd/mmc, use default value */
 	if ((boot_type != BOOT_TYPE_SD) && (boot_type != BOOT_TYPE_MMC))
-		return CONFIG_ENV_MMC_DEVICE_INDEX;
+		return env_get_ulong("mmcdev", 10, CONFIG_ENV_MMC_DEVICE_INDEX);
 
 	return board_mmc_get_env_dev(devno);
 }