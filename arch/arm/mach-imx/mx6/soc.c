nxp-imx/uboot-imx

MLK-19059-1 fastboot support all partiton for mx6
Base on MLK-18406: uuu write to any positionn of mmc
Support i.MX6 boards.

Signed-off-by: Xiaoning Wang <xiaoning.wang@nxp.com>

arch/arm/mach-imx/mx6/soc.c
@@ -685,7 +685,7 @@ int mmc_get_env_dev(void)
 
 	/* If not boot from sd/mmc, use default value */
 	if (devno < 0)
-		return CONFIG_SYS_MMC_ENV_DEV;
+	    return env_get_ulong("mmcdev", 10, CONFIG_SYS_MMC_ENV_DEV);
 
 	return board_mmc_get_env_dev(devno);
 }

rafluan/u-boot

(cherry picked from commit 808ccb44593b58552d3356d52f9ae23b7b77fd0a)
(cherry picked from commit ae4b9669cc6e2054fbf32e2e38aec3e43488cbb1)
(cherry picked from commit 6480d90bf0f709bdf30863b56de96d4bbeb97825)
(cherry picked from commit 4fc76f7e2d1446f9721d74a8e24207f153e0dff8)
(cherry picked from commit 08ed5537cb1a0ed7b671733216674a31ff91c3f6)

arch/arm/mach-imx/mx6/soc.c
@@ -29,6 +29,7 @@
 #include <asm/setup.h>
 #include <hang.h>
 #include <cpu_func.h>
+#include <env.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -671,7 +672,7 @@ int mmc_get_env_dev(void)
 
 	/* If not boot from sd/mmc, use default value */
 	if (devno < 0)
-		return CONFIG_ENV_MMC_DEVICE_INDEX;
+	    return env_get_ulong("mmcdev", 10, CONFIG_ENV_MMC_DEVICE_INDEX);
 
 	return board_mmc_get_env_dev(devno);
 }