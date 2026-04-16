nxp-imx/uboot-imx

LF-9739 crypto: jr: set random Differential Power Analysis Resistance (DPAR) Mask
randdpar bit in scfgr is used to reseed the AESA DPAR Mask
from RNG DRNG State Handle 0. This bit must be set
for normal runtime mode.

Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
Reviewed-by: Frank Zhang <frank.zhang@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>

drivers/crypto/fsl/jr.c
@@ -788,6 +788,11 @@ static int rng_init(uint8_t sec_idx, ccsr_sec_t *sec)
 	 /* Enable RDB bit so that RNG works faster */
 	sec_setbits32(&sec->scfgr, SEC_SCFGR_RDBENABLE);
 
+	if (IS_ENABLED(CONFIG_SPL_BUILD) && IS_ENABLED(CONFIG_IMX8ULP)) {
+		/* AESA DPAR Mask is reseeded from RNG DRNG State Handle 0 */
+		sec_setbits32(&sec->scfgr, SEC_SCFGR_RANDDPAR);
+	}
+
 	return ret;
 }
 

rafluan/u-boot

drivers/crypto/fsl/jr.c
@@ -788,6 +788,11 @@ static int rng_init(uint8_t sec_idx, ccsr_sec_t *sec)
 	 /* Enable RDB bit so that RNG works faster */
 	sec_setbits32(&sec->scfgr, SEC_SCFGR_RDBENABLE);
 
+	if (IS_ENABLED(CONFIG_XPL_BUILD) && IS_ENABLED(CONFIG_IMX8ULP)) {
+		/* AESA DPAR Mask is reseeded from RNG DRNG State Handle 0 */
+		sec_setbits32(&sec->scfgr, SEC_SCFGR_RANDDPAR);
+	}
+
 	return ret;
 }
 