nxp-imx/uboot-imx

LF-2637 mx6: fuse: update node path
Update node path for 5.10 Kernel.
 - aips-bus renamed to bus
 - gpmi-nand renamed to nand-controller

Reviewed-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

arch/arm/mach-imx/mx6/module_fuse.c
@@ -22,6 +22,45 @@ struct fuse_entry_desc {
 
 static struct fuse_entry_desc mx6_fuse_descs[] = {
 #if defined(CONFIG_MX6ULL)
+	{MX6_MODULE_TSC, "/soc/bus@2000000/tsc@2040000", 0x430, 22},
+	{MX6_MODULE_ADC2, "/soc/bus@2100000/adc@219c000", 0x430, 23},
+	{MX6_MODULE_EPDC, "/soc/bus@2200000/epdc@228c000", 0x430, 24},
+	{MX6_MODULE_ESAI, "/soc/bus@2000000/spba-bus@2000000/esai@2024000", 0x430, 25},
+	{MX6_MODULE_FLEXCAN1, "/soc/bus@2000000/can@2090000", 0x430, 26},
+	{MX6_MODULE_FLEXCAN2, "/soc/bus@2000000/can@2094000", 0x430, 27},
+	{MX6_MODULE_SPDIF, "/soc/bus@2000000/spba-bus@2000000/spdif@2004000", 0x440, 2},
+	{MX6_MODULE_EIM, "/soc/bus@2100000/weim@21b8000", 0x440, 3},
+	{MX6_MODULE_SD1, "/soc/bus@2100000/usdhc@2190000", 0x440, 4},
+	{MX6_MODULE_SD2, "/soc/bus@2100000/usdhc@2194000", 0x440, 5},
+	{MX6_MODULE_QSPI1, "/soc/bus@2100000/qspi@21e0000", 0x440, 6},
+	{MX6_MODULE_GPMI, "/soc/nand-controller@1806000", 0x440, 7},
+	{MX6_MODULE_APBHDMA, "/soc/dma-apbh@1804000", 0x440, 7},
+	{MX6_MODULE_LCDIF, "/soc/bus@2100000/lcdif@21c8000", 0x440, 8},
+	{MX6_MODULE_PXP, "/soc/bus@2100000/pxp@21cc000", 0x440, 9},
+	{MX6_MODULE_CSI, "/soc/bus@2100000/csi@21c4000", 0x440, 10},
+	{MX6_MODULE_ADC1, "/soc/bus@2100000/adc@2198000", 0x440, 11},
+	{MX6_MODULE_ENET1, "/soc/bus@2100000/ethernet@2188000", 0x440, 12},
+	{MX6_MODULE_ENET2, "/soc/bus@2000000/ethernet@20b4000", 0x440, 13},
+	{MX6_MODULE_DCP, "/soc/bus@2200000/dcp@2280000", 0x440, 14},
+	{MX6_MODULE_USB_OTG2, "/soc/bus@2100000/usb@2184200", 0x440, 15},
+	{MX6_MODULE_SAI2, "/soc/bus@2000000/spba-bus@2000000/sai@202c000", 0x440, 24},
+	{MX6_MODULE_SAI3, "/soc/bus@2000000/spba-bus@2000000/sai@2030000", 0x440, 24},
+	{MX6_MODULE_DCP_CRYPTO, "/soc/bus@2200000/dcp@2280000", 0x440, 25},
+	{MX6_MODULE_UART5, "/soc/bus@2100000/serial@21f4000", 0x440, 26},
+	{MX6_MODULE_UART6, "/soc/bus@2100000/serial@21fc000", 0x440, 26},
+	{MX6_MODULE_UART7, "/soc/bus@2000000/spba-bus@2000000/serial@2018000", 0x440, 26},
+	{MX6_MODULE_UART8, "/soc/bus@2200000/serial@2288000", 0x440, 26},
+	{MX6_MODULE_PWM5, "/soc/bus@2000000/pwm@20f0000", 0x440, 27},
+	{MX6_MODULE_PWM6, "/soc/bus@2000000/pwm@20f4000", 0x440, 27},
+	{MX6_MODULE_PWM7, "/soc/bus@2000000/pwm@20f8000", 0x440, 27},
+	{MX6_MODULE_PWM8, "/soc/bus@2000000/pwm@20fc000", 0x440, 27},
+	{MX6_MODULE_ECSPI3, "/soc/bus@2000000/spba-bus@2000000/ecspi@2010000", 0x440, 28},
+	{MX6_MODULE_ECSPI4, "/soc/bus@2000000/spba-bus@2000000/ecspi@2014000", 0x440, 28},
+	{MX6_MODULE_I2C3, "/soc/bus@2100000/i2c@21a8000", 0x440, 29},
+	{MX6_MODULE_I2C4, "/soc/bus@2100000/i2c@21f8000", 0x440, 29},
+	{MX6_MODULE_GPT2, "/soc/bus@2000000/gpt@20e8000", 0x440, 30},
+	{MX6_MODULE_EPIT2, "/soc/bus@2000000/epit@20d4000", 0x440, 31},
+
 	{MX6_MODULE_TSC, "/soc/aips-bus@2000000/tsc@2040000", 0x430, 22},
 	{MX6_MODULE_ADC2, "/soc/aips-bus@2100000/adc@219c000", 0x430, 23},
 	{MX6_MODULE_EPDC, "/soc/aips-bus@2200000/epdc@228c000", 0x430, 24},
@@ -100,6 +139,45 @@ static struct fuse_entry_desc mx6_fuse_descs[] = {
 	{MX6_MODULE_GPT2, "/soc/aips-bus@02000000/gpt@020e8000", 0x440, 30},
 	{MX6_MODULE_EPIT2, "/soc/aips-bus@02000000/epit@020d4000", 0x440, 31},
 #elif defined(CONFIG_MX6UL)
+	{MX6_MODULE_TSC, "/soc/bus@2000000/tsc@2040000", 0x430, 22},
+	{MX6_MODULE_ADC2, "/soc/bus@2100000/adc@219c000", 0x430, 23},
+	{MX6_MODULE_SIM1, "/soc/bus@2100000/sim@218c000", 0x430, 24},
+	{MX6_MODULE_SIM2, "/soc/bus@2100000/sim@21b4000", 0x430, 25},
+	{MX6_MODULE_FLEXCAN1, "/soc/bus@2000000/can@2090000", 0x430, 26},
+	{MX6_MODULE_FLEXCAN2, "/soc/bus@2000000/can@2094000", 0x430, 27},
+	{MX6_MODULE_SPDIF, "/soc/bus@2000000/spba-bus@2000000/spdif@2004000", 0x440, 2},
+	{MX6_MODULE_EIM, "/soc/bus@2100000/weim@21b8000", 0x440, 3},
+	{MX6_MODULE_SD1, "/soc/bus@2100000/usdhc@2190000", 0x440, 4},
+	{MX6_MODULE_SD2, "/soc/bus@2100000/usdhc@2194000", 0x440, 5},
+	{MX6_MODULE_QSPI1, "/soc/bus@2100000/qspi@21e0000", 0x440, 6},
+	{MX6_MODULE_GPMI, "/soc/nand-controller@1806000", 0x440, 7},
+	{MX6_MODULE_APBHDMA, "/soc/dma-apbh@1804000", 0x440, 7},
+	{MX6_MODULE_LCDIF, "/soc/bus@2100000/lcdif@21c8000", 0x440, 8},
+	{MX6_MODULE_PXP, "/soc/bus@2100000/pxp@21cc000", 0x440, 9},
+	{MX6_MODULE_CSI, "/soc/bus@2100000/csi@21c4000", 0x440, 10},
+	{MX6_MODULE_ADC1, "/soc/bus@2100000/adc@2198000", 0x440, 11},
+	{MX6_MODULE_ENET1, "/soc/bus@2100000/ethernet@2188000", 0x440, 12},
+	{MX6_MODULE_ENET2, "/soc/bus@2000000/ethernet@20b4000", 0x440, 13},
+	{MX6_MODULE_CAAM, "/soc/bus@2100000/caam@2140000", 0x440, 14},
+	{MX6_MODULE_USB_OTG2, "/soc/bus@2100000/usb@2184200", 0x440, 15},
+	{MX6_MODULE_SAI2, "/soc/bus@2000000/spba-bus@2000000/sai@202c000", 0x440, 24},
+	{MX6_MODULE_SAI3, "/soc/bus@2000000/spba-bus@2000000/sai@2030000", 0x440, 24},
+	{MX6_MODULE_BEE, "/soc/bus@2000000/bee@2044000", 0x440, 25},
+	{MX6_MODULE_UART5, "/soc/bus@2100000/serial@21f4000", 0x440, 26},
+	{MX6_MODULE_UART6, "/soc/bus@2100000/serial@21fc000", 0x440, 26},
+	{MX6_MODULE_UART7, "/soc/bus@2000000/spba-bus@2000000/serial@2018000", 0x440, 26},
+	{MX6_MODULE_UART8, "/soc/bus@2000000/spba-bus@2000000/serial@2024000", 0x440, 26},
+	{MX6_MODULE_PWM5, "/soc/bus@2000000/pwm@20f0000", 0x440, 27},
+	{MX6_MODULE_PWM6, "/soc/bus@2000000/pwm@20f4000", 0x440, 27},
+	{MX6_MODULE_PWM7, "/soc/bus@2000000/pwm@20f8000", 0x440, 27},
+	{MX6_MODULE_PWM8, "/soc/bus@2000000/pwm@20fc000", 0x440, 27},
+	{MX6_MODULE_ECSPI3, "/soc/bus@2000000/spba-bus@2000000/ecspi@2010000", 0x440, 28},
+	{MX6_MODULE_ECSPI4, "/soc/bus@2000000/spba-bus@2000000/ecspi@2014000", 0x440, 28},
+	{MX6_MODULE_I2C3, "/soc/bus@2100000/i2c@21a8000", 0x440, 29},
+	{MX6_MODULE_I2C4, "/soc/bus@2100000/i2c@21f8000", 0x440, 29},
+	{MX6_MODULE_GPT2, "/soc/bus@2000000/gpt@20e8000", 0x440, 30},
+	{MX6_MODULE_EPIT2, "/soc/bus@2000000/epit@20d4000", 0x440, 31},
+
 	{MX6_MODULE_TSC, "/soc/aips-bus@2000000/tsc@2040000", 0x430, 22},
 	{MX6_MODULE_ADC2, "/soc/aips-bus@2100000/adc@219c000", 0x430, 23},
 	{MX6_MODULE_SIM1, "/soc/aips-bus@2100000/sim@218c000", 0x430, 24},

rafluan/u-boot

(cherry picked from commit 51ee59aa02cb0a88c64604faecd5b01022af5182)
(cherry picked from commit f7fba141b386d3dfe4b1862c5ae088e65a17efa7)
(cherry picked from commit 2035fbbd23963fcb475c615c630c914b1b519549)
(cherry picked from commit 2f934fa855248a98572047dc0f05901dac7c58ac)

arch/arm/mach-imx/mx6/module_fuse.c
@@ -12,6 +12,45 @@
 
 static struct fuse_entry_desc mx6_fuse_descs[] = {
 #if defined(CONFIG_MX6ULL)
+	{MODULE_TSC, "/soc/bus@2000000/tsc@2040000", 0x430, 22},
+	{MODULE_ADC2, "/soc/bus@2100000/adc@219c000", 0x430, 23},
+	{MODULE_EPDC, "/soc/bus@2200000/epdc@228c000", 0x430, 24},
+	{MODULE_ESAI, "/soc/bus@2000000/spba-bus@2000000/esai@2024000", 0x430, 25},
+	{MODULE_FLEXCAN1, "/soc/bus@2000000/can@2090000", 0x430, 26},
+	{MODULE_FLEXCAN2, "/soc/bus@2000000/can@2094000", 0x430, 27},
+	{MODULE_SPDIF, "/soc/bus@2000000/spba-bus@2000000/spdif@2004000", 0x440, 2},
+	{MODULE_EIM, "/soc/bus@2100000/weim@21b8000", 0x440, 3},
+	{MODULE_SD1, "/soc/bus@2100000/usdhc@2190000", 0x440, 4},
+	{MODULE_SD2, "/soc/bus@2100000/usdhc@2194000", 0x440, 5},
+	{MODULE_QSPI1, "/soc/bus@2100000/qspi@21e0000", 0x440, 6},
+	{MODULE_GPMI, "/soc/nand-controller@1806000", 0x440, 7},
+	{MODULE_APBHDMA, "/soc/dma-apbh@1804000", 0x440, 7},
+	{MODULE_LCDIF, "/soc/bus@2100000/lcdif@21c8000", 0x440, 8},
+	{MODULE_PXP, "/soc/bus@2100000/pxp@21cc000", 0x440, 9},
+	{MODULE_CSI, "/soc/bus@2100000/csi@21c4000", 0x440, 10},
+	{MODULE_ADC1, "/soc/bus@2100000/adc@2198000", 0x440, 11},
+	{MODULE_ENET1, "/soc/bus@2100000/ethernet@2188000", 0x440, 12},
+	{MODULE_ENET2, "/soc/bus@2000000/ethernet@20b4000", 0x440, 13},
+	{MODULE_DCP, "/soc/bus@2200000/dcp@2280000", 0x440, 14},
+	{MODULE_USB_OTG2, "/soc/bus@2100000/usb@2184200", 0x440, 15},
+	{MODULE_SAI2, "/soc/bus@2000000/spba-bus@2000000/sai@202c000", 0x440, 24},
+	{MODULE_SAI3, "/soc/bus@2000000/spba-bus@2000000/sai@2030000", 0x440, 24},
+	{MODULE_DCP_CRYPTO, "/soc/bus@2200000/dcp@2280000", 0x440, 25},
+	{MODULE_UART5, "/soc/bus@2100000/serial@21f4000", 0x440, 26},
+	{MODULE_UART6, "/soc/bus@2100000/serial@21fc000", 0x440, 26},
+	{MODULE_UART7, "/soc/bus@2000000/spba-bus@2000000/serial@2018000", 0x440, 26},
+	{MODULE_UART8, "/soc/bus@2200000/serial@2288000", 0x440, 26},
+	{MODULE_PWM5, "/soc/bus@2000000/pwm@20f0000", 0x440, 27},
+	{MODULE_PWM6, "/soc/bus@2000000/pwm@20f4000", 0x440, 27},
+	{MODULE_PWM7, "/soc/bus@2000000/pwm@20f8000", 0x440, 27},
+	{MODULE_PWM8, "/soc/bus@2000000/pwm@20fc000", 0x440, 27},
+	{MODULE_ECSPI3, "/soc/bus@2000000/spba-bus@2000000/ecspi@2010000", 0x440, 28},
+	{MODULE_ECSPI4, "/soc/bus@2000000/spba-bus@2000000/ecspi@2014000", 0x440, 28},
+	{MODULE_I2C3, "/soc/bus@2100000/i2c@21a8000", 0x440, 29},
+	{MODULE_I2C4, "/soc/bus@2100000/i2c@21f8000", 0x440, 29},
+	{MODULE_GPT2, "/soc/bus@2000000/gpt@20e8000", 0x440, 30},
+	{MODULE_EPIT2, "/soc/bus@2000000/epit@20d4000", 0x440, 31},
+
 	{MODULE_TSC, "/soc/aips-bus@2000000/tsc@2040000", 0x430, 22},
 	{MODULE_ADC2, "/soc/aips-bus@2100000/adc@219c000", 0x430, 23},
 	{MODULE_EPDC, "/soc/aips-bus@2200000/epdc@228c000", 0x430, 24},
@@ -90,6 +129,45 @@ static struct fuse_entry_desc mx6_fuse_descs[] = {
 	{MODULE_GPT2, "/soc/aips-bus@02000000/gpt@020e8000", 0x440, 30},
 	{MODULE_EPIT2, "/soc/aips-bus@02000000/epit@020d4000", 0x440, 31},
 #elif defined(CONFIG_MX6UL)
+	{MODULE_TSC, "/soc/bus@2000000/tsc@2040000", 0x430, 22},
+	{MODULE_ADC2, "/soc/bus@2100000/adc@219c000", 0x430, 23},
+	{MODULE_SIM1, "/soc/bus@2100000/sim@218c000", 0x430, 24},
+	{MODULE_SIM2, "/soc/bus@2100000/sim@21b4000", 0x430, 25},
+	{MODULE_FLEXCAN1, "/soc/bus@2000000/can@2090000", 0x430, 26},
+	{MODULE_FLEXCAN2, "/soc/bus@2000000/can@2094000", 0x430, 27},
+	{MODULE_SPDIF, "/soc/bus@2000000/spba-bus@2000000/spdif@2004000", 0x440, 2},
+	{MODULE_EIM, "/soc/bus@2100000/weim@21b8000", 0x440, 3},
+	{MODULE_SD1, "/soc/bus@2100000/usdhc@2190000", 0x440, 4},
+	{MODULE_SD2, "/soc/bus@2100000/usdhc@2194000", 0x440, 5},
+	{MODULE_QSPI1, "/soc/bus@2100000/qspi@21e0000", 0x440, 6},
+	{MODULE_GPMI, "/soc/nand-controller@1806000", 0x440, 7},
+	{MODULE_APBHDMA, "/soc/dma-apbh@1804000", 0x440, 7},
+	{MODULE_LCDIF, "/soc/bus@2100000/lcdif@21c8000", 0x440, 8},
+	{MODULE_PXP, "/soc/bus@2100000/pxp@21cc000", 0x440, 9},
+	{MODULE_CSI, "/soc/bus@2100000/csi@21c4000", 0x440, 10},
+	{MODULE_ADC1, "/soc/bus@2100000/adc@2198000", 0x440, 11},
+	{MODULE_ENET1, "/soc/bus@2100000/ethernet@2188000", 0x440, 12},
+	{MODULE_ENET2, "/soc/bus@2000000/ethernet@20b4000", 0x440, 13},
+	{MODULE_CAAM, "/soc/bus@2100000/caam@2140000", 0x440, 14},
+	{MODULE_USB_OTG2, "/soc/bus@2100000/usb@2184200", 0x440, 15},
+	{MODULE_SAI2, "/soc/bus@2000000/spba-bus@2000000/sai@202c000", 0x440, 24},
+	{MODULE_SAI3, "/soc/bus@2000000/spba-bus@2000000/sai@2030000", 0x440, 24},
+	{MODULE_BEE, "/soc/bus@2000000/bee@2044000", 0x440, 25},
+	{MODULE_UART5, "/soc/bus@2100000/serial@21f4000", 0x440, 26},
+	{MODULE_UART6, "/soc/bus@2100000/serial@21fc000", 0x440, 26},
+	{MODULE_UART7, "/soc/bus@2000000/spba-bus@2000000/serial@2018000", 0x440, 26},
+	{MODULE_UART8, "/soc/bus@2000000/spba-bus@2000000/serial@2024000", 0x440, 26},
+	{MODULE_PWM5, "/soc/bus@2000000/pwm@20f0000", 0x440, 27},
+	{MODULE_PWM6, "/soc/bus@2000000/pwm@20f4000", 0x440, 27},
+	{MODULE_PWM7, "/soc/bus@2000000/pwm@20f8000", 0x440, 27},
+	{MODULE_PWM8, "/soc/bus@2000000/pwm@20fc000", 0x440, 27},
+	{MODULE_ECSPI3, "/soc/bus@2000000/spba-bus@2000000/ecspi@2010000", 0x440, 28},
+	{MODULE_ECSPI4, "/soc/bus@2000000/spba-bus@2000000/ecspi@2014000", 0x440, 28},
+	{MODULE_I2C3, "/soc/bus@2100000/i2c@21a8000", 0x440, 29},
+	{MODULE_I2C4, "/soc/bus@2100000/i2c@21f8000", 0x440, 29},
+	{MODULE_GPT2, "/soc/bus@2000000/gpt@20e8000", 0x440, 30},
+	{MODULE_EPIT2, "/soc/bus@2000000/epit@20d4000", 0x440, 31},
+
 	{MODULE_TSC, "/soc/aips-bus@2000000/tsc@2040000", 0x430, 22},
 	{MODULE_ADC2, "/soc/aips-bus@2100000/adc@219c000", 0x430, 23},
 	{MODULE_SIM1, "/soc/aips-bus@2100000/sim@218c000", 0x430, 24},
@@ -128,6 +206,7 @@ static struct fuse_entry_desc mx6_fuse_descs[] = {
 	{MODULE_I2C4, "/soc/aips-bus@2100000/i2c@21f8000", 0x440, 29},
 	{MODULE_GPT2, "/soc/aips-bus@2000000/gpt@20e8000", 0x440, 30},
 	{MODULE_EPIT2, "/soc/aips-bus@2000000/epit@20d4000", 0x440, 31},
+
 	/* Paths for older imx tree: */
 	{MODULE_TSC, "/soc/aips-bus@02000000/tsc@02040000", 0x430, 22},
 	{MODULE_ADC2, "/soc/aips-bus@02100000/adc@0219c000", 0x430, 23},