nxp-imx/uboot-imx

MLK-24817 iMX8MN: Add iMX8MN 11x11 DDR3L EVK support
Add relevant configs and DTS for iMX8MN 11x11 DDR3L EVK in SPL/u-boot.
Compared with LPDDR4 EVK, the DDR3L EVK has below changes:
1. DDR is changed to 1GB DDR3L
2. WIFI chip is changed.
3. Power rails changed:
   a) VDD_ARM is combined to VDD_SOC, so disable buck2.
   b) NVCC_DRAM needs 1.35v for DDR3L.
   c) VDD_SOC sets to 0.85v since DDR runs at 1600MTS
4. NO GPU and MIPI display. Splash screen is disabled.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

board/freescale/imx8mn_evk/spl.c
@@ -192,6 +192,15 @@ int power_init_board(void)
 #ifdef CONFIG_IMX8MN_LOW_DRIVE_MODE
 	/* Set VDD_SOC/VDD_DRAM to 0.8v for low drive mode */
 	pmic_reg_write(p, PCA9450_BUCK1OUT_DVS0, 0x10);
+#elif defined(CONFIG_TARGET_IMX8MN_DDR3_EVK)
+	/* Set VDD_SOC to 0.85v for DDR3L at 1600MTS */
+	pmic_reg_write(p, PCA9450_BUCK1OUT_DVS0, 0x14);
+
+	/* Disable the BUCK2 */
+	pmic_reg_write(p, PCA9450_BUCK2CTRL, 0x48);
+
+	/* Set NVCC_DRAM to 1.35v */
+	pmic_reg_write(p, PCA9450_BUCK6OUT, 0x1E);
 #else
 	/* increase VDD_SOC/VDD_DRAM to typical value 0.95V before first DRAM access */
 	pmic_reg_write(p, PCA9450_BUCK1OUT_DVS0, 0x1C);

rafluan/u-boot

(cherry picked from commit 752ab54b9cb983efcf1ea4bf68e87fb7b837a811)
(cherry picked from commit 6da7ba4c30e99d8716985fb38844e9b6420f92c2)
(cherry picked from commit 73fa747e96390fcb214144a986873b8d18f0b999)
(cherry picked from commit 5d59c2f393dd8eaca448868f6523f38496194273)

board/nxp/imx8mn_evk/spl.c
@@ -96,6 +96,15 @@ int power_init_board(void)
 #ifdef CONFIG_IMX8MN_LOW_DRIVE_MODE
 	/* Set VDD_SOC/VDD_DRAM to 0.8v for low drive mode */
 	pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, 0x10);
+#elif defined(CONFIG_TARGET_IMX8MN_DDR3_EVK)
+	/* Set VDD_SOC to 0.85v for DDR3L at 1600MTS */
+	pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, 0x14);
+
+	/* Disable the BUCK2 */
+	pmic_reg_write(dev, PCA9450_BUCK2CTRL, 0x48);
+
+	/* Set NVCC_DRAM to 1.35v */
+	pmic_reg_write(dev, PCA9450_BUCK6OUT, 0x1E);
 #else
 	/* increase VDD_SOC/VDD_DRAM to typical value 0.95V before first DRAM access */
 	pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, 0x1C);