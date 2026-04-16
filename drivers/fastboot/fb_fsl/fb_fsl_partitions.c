nxp-imx/uboot-imx

MLK-22293-4 fastboot: Change to use SCSI interface for sata device
After enabling AHCI driver and DM SCSI, we need to access SCSI device
for SATA

Signed-off-by: Ye Li <ye.li@nxp.com>

drivers/fastboot/fb_fsl/fb_fsl_partitions.c
@@ -18,8 +18,8 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/setup.h>
 #include <environment.h>
-#ifdef CONFIG_SATA
-#include <sata.h>
+#ifdef CONFIG_DM_SCSI
+#include <scsi.h>
 #endif
 
 #if defined(CONFIG_FASTBOOT_LOCK)
@@ -147,17 +147,11 @@ static int _fastboot_parts_load_from_ptable(void)
 
 	/* sata case in env */
 	if (fastboot_devinfo.type == DEV_SATA) {
-#ifdef CONFIG_SATA
+#ifdef CONFIG_DM_SCSI
 		int sata_device_no = fastboot_devinfo.dev_id;
 		puts("flash target is SATA\n");
-		if (sata_initialize())
-			return -1;
-		if (sata_device_no >= CONFIG_SYS_SATA_MAX_DEVICE) {
-			printf("Unknown SATA(%d) device for fastboot\n",
-				sata_device_no);
-			return -1;
-		}
-		dev_desc = sata_get_dev(sata_device_no);
+		scsi_scan(false);
+		dev_desc = blk_get_dev("scsi", sata_device_no);
 #else /*! CONFIG_SATA*/
 		puts("SATA isn't buildin\n");
 		return -1;

rafluan/u-boot

(cherry picked from commit 764cc673d345abf775ed9d05737a3dd856b5dfde)
(cherry picked from commit 92e4cc082eef6b59fb3f723b3362d17a06b3389a)
(cherry picked from commit f2fb48d5190fbab1f6f5e50afa1c9e9a1acc076e)
(cherry picked from commit ad2479136e60c36b9fe219f26e6a2c09f1106163)

drivers/fastboot/fb_fsl/fb_fsl_partitions.c
@@ -18,8 +18,8 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/setup.h>
 #include <env.h>
-#ifdef CONFIG_SATA
-#include <sata.h>
+#ifdef CONFIG_SCSI
+#include <scsi.h>
 #endif
 
 #if defined(CONFIG_FASTBOOT_LOCK)
@@ -147,17 +147,11 @@ static int _fastboot_parts_load_from_ptable(void)
 
 	/* sata case in env */
 	if (fastboot_devinfo.type == DEV_SATA) {
-#ifdef CONFIG_SATA
+#ifdef CONFIG_SCSI
 		int sata_device_no = fastboot_devinfo.dev_id;
 		puts("flash target is SATA\n");
-		if (sata_initialize())
-			return -1;
-		if (sata_device_no >= CONFIG_SYS_SATA_MAX_DEVICE) {
-			printf("Unknown SATA(%d) device for fastboot\n",
-				sata_device_no);
-			return -1;
-		}
-		dev_desc = sata_get_dev(sata_device_no);
+		scsi_scan(false);
+		dev_desc = blk_get_dev("scsi", sata_device_no);
 #else /*! CONFIG_SATA*/
 		puts("SATA isn't buildin\n");
 		return -1;