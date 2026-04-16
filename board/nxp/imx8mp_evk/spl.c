nxp-imx/uboot-imx

MLK-24971-1 imx8mp_evk: Change to nominal voltage on DDR4 EVK
Because DDR4 runs at 3200MTS, this speed does not require to use overdrive
voltage for VDD_SOC, so set VDD_SOC to nominal 0.85v on DDR4 EVK.
The VDD_ARM was set to 0.95v to avoid timing risk from SOC to ARM when
VDD_SOC is 0.95v, set it back to 0.85v as well.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

board/freescale/imx8mp_evk/spl.c
@@ -187,6 +187,7 @@ int power_init_board(void)
 	/* BUCKxOUT_DVS0/1 control BUCK123 output */
 	pmic_reg_write(p, PCA9450_BUCK123_DVS, 0x29);
 
+#ifdef CONFIG_IMX8M_LPDDR4
 	/*
 	 * increase VDD_SOC to typical value 0.95V before first
 	 * DRAM access, set DVS1 to 0.85v for suspend.
@@ -200,8 +201,10 @@ int power_init_board(void)
 	/* Kernel uses OD/OD freq for SOC */
 	/* To avoid timing risk from SOC to ARM,increase VDD_ARM to OD voltage 0.95v */
 	pmic_reg_write(p, PCA9450_BUCK2OUT_DVS0, 0x1C);
+#elif defined(CONFIG_IMX8M_DDR4)
+	/* DDR4 runs at 3200MTS, uses default ND 0.85v for VDD_SOC and VDD_ARM */
+	pmic_reg_write(p, PCA9450_BUCK1CTRL, 0x59);
 
-#ifdef CONFIG_IMX8M_DDR4
 	/* Set NVCC_DRAM to 1.2v for DDR4 */
 	pmic_reg_write(p, PCA9450_BUCK6OUT, 0x18);
 #endif

rafluan/u-boot

(cherry picked from commit 5d556b8e214739deb5c3c71efa2ea8fc28f2a643)
(cherry picked from commit 18c1850717678a6a701641cc3d6d740624b2560e)
(cherry picked from commit ef40b1e81d6a9a7245e465be96fd468964f90363)
(cherry picked from commit 2de6d07d77334e382be826cd84344c9fd31427e9)

board/nxp/imx8mp_evk/spl.c
@@ -66,6 +66,7 @@ int power_init_board(void)
 	/* BUCKxOUT_DVS0/1 control BUCK123 output */
 	pmic_reg_write(dev, PCA9450_BUCK123_DVS, 0x29);
 
+#ifdef CONFIG_IMX8M_LPDDR4
 	/*
 	 * Increase VDD_SOC to typical value 0.95V before first
 	 * DRAM access, set DVS1 to 0.85V for suspend.
@@ -87,8 +88,10 @@ int power_init_board(void)
 	 */
 
 	pmic_reg_write(dev, PCA9450_BUCK2OUT_DVS0, 0x1C);
+#elif defined(CONFIG_IMX8M_DDR4)
+	/* DDR4 runs at 3200MTS, uses default ND 0.85v for VDD_SOC and VDD_ARM */
+	pmic_reg_write(dev, PCA9450_BUCK1CTRL, 0x59);
 
-#ifdef CONFIG_IMX8M_DDR4
 	/* Set NVCC_DRAM to 1.2v for DDR4 */
 	pmic_reg_write(dev, PCA9450_BUCK6OUT, 0x18);
 #endif