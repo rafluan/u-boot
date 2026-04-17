nxp-imx/uboot-imx

LFU-958-8 ddr: phy: Add DDR quick boot dependency for iMX952
iMX952 also supports DDR quick boot, add the dependency to DDR QB
relevant configs and default enable qb command.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

arch/arm/include/asm/arch-imx9/ddr.h
@@ -129,7 +129,7 @@ int ddrphy_qb_save(void);
 int ddr_cfg_phy_qb(struct dram_timing_info *timing_info, int fsp_id);
 #endif
 #endif
-#elif defined(CONFIG_IMX95)	|| defined(CONFIG_IMX94) /* CONFIG_IMX95 || CONFIG_IMX94 */
+#elif defined(CONFIG_IMX95)	|| defined(CONFIG_IMX94) || defined(CONFIG_IMX952) /* iMX95, iMX94, iMX952 */
 #if   defined(CONFIG_IMX_SNPS_DDR_PHY_QB_GEN)
 /* Quick Boot related */
 #define DDRPHY_QB_CSR_SIZE	5168

rafluan/u-boot

arch/arm/include/asm/arch-imx9/ddr.h
@@ -130,7 +130,7 @@ int ddr_cfg_phy_qb(struct dram_timing_info *timing_info, int fsp_id);
 #endif
 #endif
 
-#elif IS_ENABLED(CONFIG_IMX95) || IS_ENABLED(CONFIG_IMX94) /* CONFIG_IMX95 || CONFIG_IMX94 */
+#elif IS_ENABLED(CONFIG_IMX95) || IS_ENABLED(CONFIG_IMX94) || IS_ENABLED(CONFIG_IMX952) /* iMX95, iMX94, iMX952 */
 #if IS_ENABLED(CONFIG_IMX_SNPS_DDR_PHY_QB_GEN)
 /* Quick Boot related */
 #define DDRPHY_QB_CSR_SIZE	5168