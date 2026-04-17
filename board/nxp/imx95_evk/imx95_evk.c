varigit/uboot-imx

LFU-732 imx95: power on m7 for kick m7 in A55
OEI power off M7 if no m7 image in flash.bin, this breaks mx95alt to let
uboot kick M7. So need power on M7 in uboot stage when need AP to manage
M7.

Reviewed-by: Joy Zou <joy.zou@nxp.com>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

board/freescale/imx95_evk/imx95_evk.c
@@ -20,7 +20,7 @@
 #include <miiphy.h>
 #include <netdev.h>
 #include <asm/gpio.h>
-#include <asm/mach-imx/sys_proto.h>
+#include <asm/arch/sys_proto.h>
 #include <i2c.h>
 
 #ifdef CONFIG_SCMI_FIRMWARE
@@ -355,6 +355,9 @@ int board_init(void)
 #endif
 
 	netc_init();
+
+	power_on_m7("mx95alt");
+
 	return 0;
 }
 

board/toradex/verdin-imx95/verdin-imx95.c
@@ -20,7 +20,7 @@
 #include <miiphy.h>
 #include <netdev.h>
 #include <asm/gpio.h>
-#include <asm/mach-imx/sys_proto.h>
+#include <asm/arch/sys_proto.h>
 #include <i2c.h>
 
 #ifdef CONFIG_SCMI_FIRMWARE
@@ -299,6 +299,8 @@ int board_init(void)
 #endif
 
 	netc_init();
+
+	power_on_m7("mx95alt");
 
 	return 0;
 }

rafluan/u-boot

board/nxp/imx95_evk/imx95_evk.c
@@ -15,10 +15,10 @@
 #include <linux/bitops.h>
 #include <linux/delay.h>
 #include <power/regulator.h>
-#include <asm/mach-imx/sys_proto.h>
 #include <scmi_agent.h>
 #include "../dts/upstream/src/arm64/freescale/imx95-power.h"
 #include <i2c.h>
+#include <asm/arch/sys_proto.h>
 
 #ifdef CONFIG_USB_TCPC
 struct tcpc_port port;
@@ -313,6 +313,9 @@ int board_init(void)
 	netc_init();
 
 	flexspi_nor_steup();
+
+	power_on_m7("mx95evkrpmsg");
+
 	return 0;
 }
 

board/toradex/verdin-imx95/verdin-imx95.c
@@ -14,9 +14,9 @@
 #include <linux/delay.h>
 #include <asm/gpio.h>
 #include <power/regulator.h>
-#include <asm/mach-imx/sys_proto.h>
 #include <scmi_agent.h>
 #include "../dts/upstream/src/arm64/freescale/imx95-power.h"
+#include <asm/arch/sys_proto.h>
 
 int board_early_init_f(void)
 {
@@ -172,6 +172,8 @@ int board_init(void)
 #endif
 
 	netc_init();
+
+	power_on_m7("mx95evkrpmsg");
 
 	return 0;
 }