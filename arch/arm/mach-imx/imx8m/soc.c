nxp-imx/uboot-imx

MA-21217 CAAM need be initialized at SPL stage
The CAAM initialize logic be changed by U-boot upgraded to 2023,
these make a failure on 8mn and 8mp evk boards. This patch make sure
The CAAM be initialized at SPL stage.

Change-Id: Ie027ba5e3c0a916f748f52fe19afe0339736859b
Signed-off-by: Maximus Sun <maximus.sun@nxp.com>

arch/arm/mach-imx/imx8m/soc.c
@@ -1775,7 +1775,7 @@ void reset_cpu(void)
 #if defined(CONFIG_ARCH_MISC_INIT)
 int arch_misc_init(void)
 {
-#ifndef CONFIG_ANDROID_SUPPORT
+#if !defined(CONFIG_ANDROID_SUPPORT) || defined(CONFIG_SPL_BUILD)
 	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
 		struct udevice *dev;
 		int ret;

rafluan/u-boot

arch/arm/mach-imx/imx8m/soc.c
@@ -1760,7 +1760,7 @@ void reset_cpu(void)
 #if IS_ENABLED(CONFIG_ARCH_MISC_INIT)
 int arch_misc_init(void)
 {
-#ifndef CONFIG_ANDROID_SUPPORT
+#if !defined(CONFIG_ANDROID_SUPPORT) || defined(CONFIG_XPL_BUILD)
 	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
 		struct udevice *dev;
 		int ret;