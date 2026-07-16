Mitec-Elettronica-Srl/uboot-imx

variscite: common: Add video code
Squashed commits:
7fb6e445acb4 variscite: Add common video code
306ee82f6f8d variscite: common: video: Get sd_dev and emmc_dev from environment

Signed-off-by: Felix Radensky <felix.r@variscite.com>
Co-authored-by: Nate Drude <nate.d@variscite.com>
(cherry picked from commit 7fb6e445acb456a7b40a7771474de352ea00b512)

board/variscite/common/video.c
@@ -1,0 +1,65 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
+ * Copyright 2020-2025 Variscite Ltd.
+ */
+
+#include <env.h>
+#include <common.h>
+#include <splash.h>
+#include <mmc.h>
+
+#ifdef CONFIG_SPLASH_SCREEN
+static int check_env(char *var, char *val)
+{
+	char *env_val = env_get(var);
+
+	if (env_val && !strcmp(env_val, val))
+		return 1;
+
+	return 0;
+}
+
+static void splash_set_source(void)
+{
+	if (!check_env("splashsourceauto", "yes"))
+		return;
+
+	if (mmc_get_env_dev() == env_get_ulong("emmc_dev", 10, 0))
+		env_set("splashsource", "emmc");
+	else if (mmc_get_env_dev() == env_get_ulong("sd_dev", 10, 0))
+		env_set("splashsource", "sd");
+}
+
+int splash_screen_prepare(void)
+{
+	char sd_devpart[5];
+	char emmc_devpart[5];
+	u32 sd_part, emmc_part;
+
+	sd_part = emmc_part = env_get_ulong("mmcpart", 10, 0);
+
+	sprintf(sd_devpart, "%ld:%d", env_get_ulong("sd_dev", 10, 0), sd_part);
+	sprintf(emmc_devpart, "%ld:%d", env_get_ulong("emmc_dev", 10, 0), emmc_part);
+
+	struct splash_location splash_locations[] = {
+		{
+			.name = "sd",
+			.storage = SPLASH_STORAGE_MMC,
+			.flags = SPLASH_STORAGE_FS,
+			.devpart = sd_devpart,
+		},
+		{
+			.name = "emmc",
+			.storage = SPLASH_STORAGE_MMC,
+			.flags = SPLASH_STORAGE_FS,
+			.devpart = emmc_devpart,
+		}
+	};
+
+	splash_set_source();
+
+	return splash_source_load(splash_locations, ARRAY_SIZE(splash_locations));
+}
+#endif

rafluan/u-boot

(cherry picked from commit 0af1c42f5ec1996749ac159010639dda08655195)
(cherry picked from commit 1b01c6cf012f80915594d1eca8f51fca1c36c6a3)
Signed-off-by: Stefano Radaelli <stefano.r@variscite.com>
(cherry picked from commit aac881129545d52d5c39c2bcb4ebd3296e613f3b)
Signed-off-by: Luan Rafael Carneiro <luan.r@variscite.com>

board/variscite/common/video.c
@@ -1,0 +1,65 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
+ * Copyright 2020-2025 Variscite Ltd.
+ */
+
+#include <vsprintf.h>
+#include <env.h>
+#include <splash.h>
+#include <mmc.h>
+
+#ifdef CONFIG_SPLASH_SCREEN
+static int check_env(char *var, char *val)
+{
+	char *env_val = env_get(var);
+
+	if (env_val && !strcmp(env_val, val))
+		return 1;
+
+	return 0;
+}
+
+static void splash_set_source(void)
+{
+	if (!check_env("splashsourceauto", "yes"))
+		return;
+
+	if (mmc_get_env_dev() == env_get_ulong("emmc_dev", 10, 0))
+		env_set("splashsource", "emmc");
+	else if (mmc_get_env_dev() == env_get_ulong("sd_dev", 10, 0))
+		env_set("splashsource", "sd");
+}
+
+int splash_screen_prepare(void)
+{
+	char sd_devpart[5];
+	char emmc_devpart[5];
+	u32 sd_part, emmc_part;
+
+	sd_part = emmc_part = env_get_ulong("mmcpart", 10, 0);
+
+	sprintf(sd_devpart, "%ld:%d", env_get_ulong("sd_dev", 10, 0), sd_part);
+	sprintf(emmc_devpart, "%ld:%d", env_get_ulong("emmc_dev", 10, 0), emmc_part);
+
+	struct splash_location splash_locations[] = {
+		{
+			.name = "sd",
+			.storage = SPLASH_STORAGE_MMC,
+			.flags = SPLASH_STORAGE_FS,
+			.devpart = sd_devpart,
+		},
+		{
+			.name = "emmc",
+			.storage = SPLASH_STORAGE_MMC,
+			.flags = SPLASH_STORAGE_FS,
+			.devpart = emmc_devpart,
+		}
+	};
+
+	splash_set_source();
+
+	return splash_source_load(splash_locations, ARRAY_SIZE(splash_locations));
+}
+#endif